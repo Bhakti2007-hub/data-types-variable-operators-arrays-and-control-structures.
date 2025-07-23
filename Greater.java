213e9e683a836aa015c6211f0094f7d460a43527
Bhakti2007-hub <bhaktisshinde3@gmail.com>
2025-07-23 20:12:38 +0530

    Greater.java

--- Greater.java
+++ Greater.java
@@ -1,24 +1,33 @@
+/*
+ * File: Greater.java
+ * Description:
+ * This program takes two integer inputs from the user and compares them
+ * to determine which number is greater. It demonstrates the use of
+ * conditional (if-else) statements and user input in Java.
+ */
+
 import java.util.*;
+
 public class Greater
 {
 	public static void main(String args[])
 	{
-		int a,b;
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter 1st number\n");
-		a=sc.nextInt();
-		System.out.println("Enter 2nd number\n");
-		b=sc.nextInt();
-		if(a>b)
+		int a, b;
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter 1st number:");
+		a = sc.nextInt();
+
+		System.out.println("Enter 2nd number:");
+		b = sc.nextInt();
+
+		if(a > b)
 		{
-			System.out.printf(a+ "is greater than" +b);
+			System.out.printf("%d is greater than %d\n", a, b);
 		}
 		else
 		{
-			System.out.printf(b+ "is greater than" +a);
-
+			System.out.printf("%d is greater than %d\n", b, a);
 		}
-
-		
 	}
 }
