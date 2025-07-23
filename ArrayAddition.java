21bb33fa890b75d056a144472400e5718236c4ec
Bhakti2007-hub <bhaktisshinde3@gmail.com>
2025-07-23 20:06:00 +0530

    ArrayAddition.java

--- ArrayAddition.java
+++ ArrayAddition.java
@@ -1,20 +1,32 @@
+/*
+ * File: ArrayAddition.java
+ * Description:
+ * This program accepts a user-defined number of integers into an array,
+ * then calculates and displays the sum of all elements.
+ * It demonstrates the use of arrays, loops, user input, and arithmetic operations in Java.
+ */
+
 import java.util.*;
+
 public class ArrayAddition
 {
 	public static void main(String args[])
 	{
 		int size;
-		System.out.println("Enter Size");
-		Scanner s=new Scanner(System.in);
-		size=s.nextInt();
-		int a[]=new int[size];
-		int i,sum=0;
-		System.out.printf("Enter the Numbers");
-		for(i=0;i<size;i++)
+		System.out.println("Enter Size:");
+		Scanner s = new Scanner(System.in);
+		size = s.nextInt();
+
+		int a[] = new int[size];
+		int sum = 0;
+
+		System.out.println("Enter the Numbers:");
+		for(int i = 0; i < size; i++)
 		{
-			a[i]=s.nextInt();
-			sum+=i;
+			a[i] = s.nextInt();
+			sum += a[i];  // Corrected: add array value, not index
 		}
-		System.out.printf("The sum is "+sum);
+
+		System.out.println("The sum is: " + sum);
 	}
 }
