26f7f94e3ee86f654c6c415ebaa04f8458954029
Bhakti2007-hub <bhaktisshinde3@gmail.com>
2025-07-23 20:23:39 +0530

    Table.java

--- Table.java
+++ Table.java
@@ -1,11 +1,18 @@
+/*
+ * File: Table.java
+ * Description:
+ * This program prints the multiplication table of 5 from 5 to 50
+ * using a for loop. It demonstrates loop control and output formatting in Java.
+ */
+
 public class Table
 {
 	public static void main(String args[])
 	{
-		System.out.println("Table is");
-		for(int i=5;i<=50;i=i+5)
+		System.out.println("Table of 5:");
+		for (int i = 5; i <= 50; i = i + 5)
 		{
-			System.out.println( +i);
+			System.out.println(i);
 		}
 	}
 }
