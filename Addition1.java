b509371201fb0811d00b5399286c2507fe258e4b
Bhakti2007-hub <bhaktisshinde3@gmail.com>
2025-07-23 20:04:51 +0530

    Addition1.java

--- Addition1.java
+++ Addition1.java
@@ -1,17 +1,30 @@
-//taking 5 numbers from user and display it
+/*
+ * File: ForLoop.java
+ * Description:
+ * This program takes 6 integer inputs from the user using a for loop
+ * and stores them in an array. Then it prints all the entered numbers.
+ * It demonstrates the use of for loop, arrays, and Scanner class in Java.
+ */
+
 import java.util.*;
+
 public class ForLoop
 {
 	public static void main (String args[])
 	{
-		Scanner s=new Scanner(System.in);
-		for(int i=0;i<=5;i=i+1)
-		{
-			System.out.printf("Enter %d element",i);
-			int a=s.nextInt();
-			
-		}
-		println("numbers are:"+a);
+		Scanner s = new Scanner(System.in);
+		int[] numbers = new int[6];
 
+		for(int i = 0; i <= 5; i++)
+		{
+			System.out.printf("Enter %d element: ", i);
+			numbers[i] = s.nextInt();
+		}
+
+		System.out.println("Numbers are:");
+		for(int i = 0; i <= 5; i++)
+		{
+			System.out.println(numbers[i]);
+		}
 	}
 }
