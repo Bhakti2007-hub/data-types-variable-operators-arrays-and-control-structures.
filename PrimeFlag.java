b81f797c5eff768d278fb55167e0d4942e9de5dc
Bhakti2007-hub <bhaktisshinde3@gmail.com>
2025-07-23 20:17:02 +0530

    PrimeFlag.java

--- PrimeFlag.java
+++ PrimeFlag.java
@@ -20,4 +20,25 @@ public class PrimeFlag
 		boolean flag = false;
 
 		if (a <= 1) {
-			flag = true; //
+			flag = true; // 0 and 1 are not prime
+		}
+
+		for (int i = 2; i <= a / 2; i++)
+		{
+			if (a % i == 0)
+			{
+				flag = true;
+				break;
+			}
+		}
+
+		if (!flag)
+		{
+			System.out.println(a + " is prime");
+		}
+		else
+		{
+			System.out.println(a + " is not prime");
+		}
+	}
+}
